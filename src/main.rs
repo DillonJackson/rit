5ece5ab4e9c069bb19cbeb75f4f61040c51066ed
Tou Yang <tuyang@ucdavis.edu>
2024-10-07 15:15:49 -0700

    accepts command args and updated dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, Rit!");
+use clap::Parser;
+// use std::env;
+use std::io;
+
+/// test functions 
+fn test1() {
+    println!("Hello from Test1");
+}
+
+fn test2() {
+    println!("Hello from Test2");
+}
+
+/// A simple CLI application.
+#[derive(Parser)]
+struct Cli {
+    /// An input argument
+    input: String,
+}
+
+fn main() -> io::Result<()> {
+    // Parsing command line arguments
+    let cli = Cli::parse();
+    
+    // Call function
+    match cli.input.as_str() {
+        "test" => test1(),
+        "test2" => test2(),
+        _ => println!("Unknown command: {}", cli.input),
+    }
+    Ok(())
 }
