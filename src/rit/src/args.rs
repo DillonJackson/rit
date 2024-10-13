9ada8836f4a3c70964bea492269a26aa4e41b1da
Dillon Jackson <81722638+DJ200148@users.noreply.github.com>
2024-10-13 12:26:30 -0700

    parser completed

---
+++ src/rit/src/args.rs
@@ -0,0 +1,27 @@
+use clap::{Args, Parser, Subcommand};
+
+// Command line interface
+#[derive(Debug, Parser)]
+#[clap(author, version, name = "Rit", about = "A simple Git like CLI application.")]
+pub struct RitArgs {
+    #[clap(subcommand)]
+    pub command: Commands
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Initialize a new repository
+    Init,
+
+    /// Remove the repository
+    Remove,
+
+    /// Store the file in the object database and return the key
+    HashObject(HashObjectCommand)
+}
+
+#[derive(Debug, Args)]
+pub struct HashObjectCommand {
+    /// The file to store
+    pub file: String
+}
--- src/rit/src/main.rs
+++ src/rit/src/main.rs
@@ -1,7 +1,13 @@
 mod utility;
 mod obj_database;
-use clap::Parser;
+mod args;
+
+
+use args::{RitArgs, Commands};
 use std::io;
+use std::path::Path;
+use clap::Parser;
+
 
 //global variables
 const DIRECTORY_PATH: &str = ".rit";
@@ -14,36 +20,49 @@ fn rit_remove() -> io::Result<()> {
 
 // initialize .rit folder
 fn rit_init() -> io::Result<()> {
+    // Check if the repository is already initialized
+    if Path::new(DIRECTORY_PATH).exists() {
+        return Err(io::Error::new(io::ErrorKind::AlreadyExists, "Repository already initialized."));
+    }
+    
     utility::init_file_structure()?;
+    println!("Repository initialized at {}.", DIRECTORY_PATH);
     Ok(())
 }
 
-/// A simple CLI application.
-#[derive(Parser)]
-struct Cli {
-    /// An input argument
-    input: String,
+// Helper function to check if the repository is initialized
+fn check_repo_initialized() -> io::Result<()> {
+    if !Path::new(DIRECTORY_PATH).exists() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "Repository not initialized. Please run `rit init` first."));
+    }
+    Ok(())
 }
 
-
 // **Usages**
+// cargo run                     -- returns the help message   
 // cargo run init                -- runs the init command
-// cargo run "repo remove"       -- runs the repo remove command
+// cargo run remove              -- runs the repo remove command
+// cargo run hash-object         -- runs the repo remove command
 
 fn main() -> io::Result<()> {
 
     // Parsing command line arguments
-    let cli = Cli::parse();
+    let args = RitArgs::parse();
     
     // calls commands
-    match cli.input.as_str() {
-        "init" => rit_init()?,
-        "repo remove" => rit_remove()?,
-        "hash" =>  match obj_database::store_data("utility.rs") {
-            Ok(key) => println!("Key: {}", key),
-            Err(e) => println!("Error: {}", e),
+    match args.command {
+        Commands::Init => {
+            rit_init()?;
+        },
+        Commands::Remove => {
+            check_repo_initialized()?;
+            rit_remove()?;
+        },
+        Commands::HashObject(hash_args) => {
+            check_repo_initialized()?;
+            obj_database::store_data(&hash_args.file)?;
         }
-        _ => println!("Unknown command: {}", cli.input),
     }
+
     Ok(())
 }
