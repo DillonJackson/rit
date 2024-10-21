5a1795b44dea25a79c34e5a43c82cdf350cb37ca
Dillon Jackson <81722638+DJ200148@users.noreply.github.com>
2024-10-20 17:38:26 -0700

    added index method to return staged changes

--- src/rit/src/index.rs
+++ src/rit/src/index.rs
@@ -15,6 +15,11 @@ pub struct IndexEntry {
     path: String
 }
 
+pub fn get_staged_files() -> io::Result<Vec<IndexEntry>> {
+    let entries = load_index()?;
+    Ok(entries)
+}
+
 pub fn get_index_path() -> PathBuf {
     Path::new(DIRECTORY_PATH).join(INDEX_FILE)
 }
