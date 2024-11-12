308195d0c8faeb7f85bb146a6b3b7ea53162c056
Dillon Jackson <81722638+DJ200148@users.noreply.github.com>
2024-11-11 17:29:57 -0800

    cleaned up the database

--- src/rit/src/database.rs
+++ src/rit/src/database.rs
@@ -47,42 +47,6 @@ pub fn store_file(file_path: &str) -> io::Result<String> {
 
     // Store the data in the object database
     store_data(&buffer)
-
-    // // Open the file in read-only mode
-    // let buffer = open_file(&file_path)?;
-    // //hash the file to obtain the key
-    // let key = match hash_file(&buffer) {
-    //     Ok(hash_value) => hash_value,
-    //     Err(e) => {
-    //         eprintln!("Error calculating hash: {}", e);
-    //         return Err(e);
-    //     },
-    // };
-
-    // // Print the stored hash value
-    // println!("SHA-256 hash: {}", key);
-
-    // //directory filename
-    // let sub_dir_name: String = key.chars().take(2).collect();
-    // // println!("SHA-256 hash first 2 char: {}", sub_dir_name);
-
-    // //file filename
-    // let filename: String = key.chars().skip(2).collect();
-    // // println!("SHA-256 hash first 2 char: {}", filename);
-
-    // // Check if the file already exists
-    // let file_path = get_object_path(&key);
-    // if std::path::Path::new(&file_path).exists() {
-    //     println!("File already exists.");
-    //     return Ok(key);
-    // }
-
-    // //creates the file
-    // let result: String = format!("{}/{}", ".rit/objects", sub_dir_name);
-    // let result_str: &str = &result;
-    // // println!("dir {}", result_str);
-    // create_file(&result_str, &filename, Some(&buffer))?;
-    // Ok(key)
 }
 
 pub fn get_data(key: &str) -> io::Result<Vec<u8>> {
@@ -96,22 +60,6 @@ pub fn get_data(key: &str) -> io::Result<Vec<u8>> {
     } else {
         Err(io::Error::new(io::ErrorKind::NotFound, "Object not found"))
     }
-
-    // let sub_dir_name: String = key.chars().take(2).collect();
-    // let filename: String = key.chars().skip(2).collect();
-    // let file_path: String = format!(".rit/objects/{}/{}", sub_dir_name, filename);
-    // let buffer = open_file(&file_path)?;
-    // match uncompress_data(&buffer) {
-    //     Ok(data) => {
-    //         if let Ok(string_data) = String::from_utf8(data.clone()) {
-    //             println!("Decompressed data: {}", string_data);
-    //         } else {
-    //             println!("Decompressed data (binary): {:?}", data);
-    //         }
-    //         Ok(data)
-    //     },
-    //     Err(e) => Err(e),
-    // }
 }
 
 pub fn delete_data(key: &str) -> io::Result<()> {
